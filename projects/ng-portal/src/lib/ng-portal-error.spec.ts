ab6fce68c14e13759072e65681f593ccc6e34b41
Nigro Simone <nigro.simone@gmail.com>
2021-03-20 14:23:18 +0100

    test

---
+++ projects/ng-portal/src/lib/ng-portal-error.spec.ts
@@ -0,0 +1,45 @@
+import { Component, DebugElement, ViewChild } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NgPortalModule } from './ng-portal.module';
+import { ngPortalInput, ngPortalOutput } from './ng-portal.decorator';
+
+@Component({selector: 'app-input', template: '<div>{{ test }}</div>'})
+class TestComponentInput {
+    @ngPortalInput() public y: any;
+}
+@Component({selector: 'app-output', template: '<div>{{ test }}</div>'})
+class TestComponentOutput {
+    @ngPortalOutput() public y: any;
+}
+@Component({template: '<app-input></app-input><app-output></app-output>'})
+class TestComponentContainer {
+    @ViewChild(TestComponentInput, {static: false}) input: TestComponentInput;
+    @ViewChild(TestComponentOutput, {static: false}) output: TestComponentOutput
+}
+describe('NgPortal error', () => {
+
+    let fixture: ComponentFixture<TestComponentContainer>;
+    let debugElement: DebugElement;
+    let element: HTMLElement;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+          declarations: [ TestComponentContainer, TestComponentInput, TestComponentOutput ],
+          imports: [NgPortalModule]
+        });
+        fixture = TestBed.createComponent(TestComponentContainer);
+        debugElement = fixture.debugElement;
+        element = debugElement.nativeElement;
+    });
+
+    afterEach(() => {
+        document.body.removeChild(element);
+    });
+
+    it('test property change', () => {
+        fixture.detectChanges();
+        expect( function(){ fixture.componentInstance.input.y.subscribe(); } ).toThrow(new Error(`Use "@ngPortalOutput({key: 'y'})" or "@ngPortal({key: 'y'})" for retrive the value`));
+        expect( function(){ fixture.componentInstance.output.y = 1; } ).toThrow(new Error(`Use "@ngPortalInput({key: 'y'})" or "@ngPortal({key: 'y'})" for send the value`));
+    });
+});
